14351412b5f8c03cfce6cc4b1ca9cbfc39fcec25
VictorIzek <victor_almanzap@outlook.com>
2023-03-27 14:34:09 -0600

    Clase temperatura

--- Sensores/temperatura.py
+++ Sensores/temperatura.py
@@ -8,6 +8,7 @@ class Temperatura(lista.Lista):
         self.sensor = Adafruit_DHT.DHT11
         super().__init__()
 
+    #Lectura
     def leer_temperatura(self):
         humedad, temperatura = Adafruit_DHT.read_retry(self.sensor, self.pin)
         if humedad is not None and temperatura is not None:
@@ -23,150 +24,60 @@ class Temperatura(lista.Lista):
             print("Error al leer los datos")
 
 
-    def diccionario_temperatura(self, clave, sensor):
+    #Diccionario
+
+    def diccionario_temperatura(self, sensor):
         temperatura = self.leer_temperatura()
         diccionario = {
-            "clave":clave,
             "valor":temperatura,
             "fecha":datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
             "sensor":sensor
         }
         return diccionario
     
-    def diccionario_humedad(self, clave, sensor):
+    def diccionario_humedad(self, sensor):
         humedad = self.leer_humedad()
         diccionario = {
-            "clave":clave,
             "valor":humedad,
             "fecha":datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
             "sensor":sensor
         }
         return diccionario
     
-    # def diccionario_temperatura(self, clave, ):
-    #     temperatura = self.leer_temperatura()
-    #     diccionario = {
-    #         "clave": clave,
-    #         "tipo":"Sensor DHT11",
-    #         "descripcion":"Sensor que mide la humedad y temperatura",
-    #         "pin":self.pin,
-    #         "valor_humedad":humedad,
-    #         "humedad_dato":"Porcentaje de Humedad",
-    #         "valor_temperatura":temperatura,
-    #         "temperatura_dato":"Grados Celcius",
-    #         "humedad":"{} %".format(humedad),
-    #         "temperatura":"{} °C".format(temperatura),
-    #         "fecha":datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    #     }
-    #     return diccionario
+
+
+    #Cargar listas
+
+    def cargar_lista_guardada_previamente(self, lista_jsons):
+        for eachJson in lista_jsons:
+            if super().cargar_lista_json(eachJson) == False:
+                return False
+            else:
+                nuevaLista = super().cargar_lista_json(eachJson)
+                for objetoIndividual in nuevaLista:
+                    diccionario  = {
+                        "valor":objetoIndividual["valor"],
+                        "fecha" : objetoIndividual["fecha"],
+                        "sensor":objetoIndividual["sensor"]
+                    }
+                    self.add(diccionario)
     
-    
-    def cargar_lista_guardada_previamente(self):
-        if super().cargar_lista_json("dht11.json") == False:
-            return False
-        else:
-            nuevaLista = super().cargar_lista_json("dht11.json")
-            for objetoIndividual in nuevaLista:
-                diccionario  = {   
-                    "clave" : objetoIndividual["clave"],
-                    "tipo" : objetoIndividual["tipo"],
-                    "descripcion" : objetoIndividual["descripcion"],
-                    "pin": objetoIndividual["pin"],
-                    "valor_humedad":objetoIndividual["valor_humedad"],
-                    "humedad_dato":objetoIndividual["humedad_dato"],
-                    "valor_temperatura":objetoIndividual["valor_temperatura"],
-                    "temperatura_dato":objetoIndividual["temperatura_dato"],
-                    "humedad":objetoIndividual["humedad"],
-                    "temperatura":objetoIndividual["temperatura"],
-                    "fecha" : objetoIndividual["fecha"],
-                }
-                self.add(diccionario)
-    
-    def cargar_lista_json_temporal(self, lista):
+    def cargar_lista_json_temporal(self, lista, listaJson):
         try:
-            listanueva = []
-            for i in lista:
-                clave = i["clave"]
-                tipo = i["tipo"]
-                descripcion = i["descripcion"]
-                pin = i["pin"]
-                valor_humedad = i["valor_humedad"]
-                humedad_dato = i["humedad_dato"]
-                valor_temperatura = i["valor_temperatura"]
-                temperatura_dato = i["temperatura_dato"]
-                humedad = i["humedad"]
-                temperatura = i["temperatura"]
-                fecha = i["fecha"]
-                listanueva.append({
-                    "clave":clave,
-                    "tipo":tipo,
-                    "descripcion":descripcion,
-                    "pin":pin,
-                    "valor_humedad":valor_humedad,
-                    "humedad_dato":humedad_dato,
-                    "valor_temperatura":valor_temperatura,
-                    "temperatura_dato":temperatura_dato,
-                    "humedad":humedad,
-                    "temperatura":temperatura,
-                    "fecha":fecha
-                    })
-            super().enviarDiccionarioYAlmacenamientoJson("dht11.json", listanueva)
-            return listanueva
+            for EachJson in listaJson:
+                listanueva = []
+                for i in lista:
+                    valor = i["valor"]
+                    fecha = i["fecha"]
+                    sensor = i["sensor"]
+                    listanueva.append({
+                        "valor":valor,
+                        "fecha":fecha,
+                        "sensor":sensor
+                        })
+                super().enviarDiccionarioYAlmacenamientoJson(EachJson, listanueva)
+                return listanueva
         except:
             return False
-        
-    def cargar_lista_guardada_previamente(self):
-        if super().cargar_lista_json("dht11.json") == False:
-            return False
-        else:
-            nuevaLista = super().cargar_lista_json("dht11.json")
-            for objetoIndividual in nuevaLista:
-                diccionario  = {   
-                    "clave" : objetoIndividual["clave"],
-                    "tipo" : objetoIndividual["tipo"],
-                    "descripcion" : objetoIndividual["descripcion"],
-                    "pin": objetoIndividual["pin"],
-                    "valor_humedad":objetoIndividual["valor_humedad"],
-                    "humedad_dato":objetoIndividual["humedad_dato"],
-                    "valor_temperatura":objetoIndividual["valor_temperatura"],
-                    "temperatura_dato":objetoIndividual["temperatura_dato"],
-                    "humedad":objetoIndividual["humedad"],
-                    "temperatura":objetoIndividual["temperatura"],
-                    "fecha" : objetoIndividual["fecha"],
-                }
-                self.add(diccionario)
-    
-    def cargar_lista_json_temporal(self, lista):
-        try:
-            listanueva = []
-            for i in lista:
-                clave = i["clave"]
-                tipo = i["tipo"]
-                descripcion = i["descripcion"]
-                pin = i["pin"]
-                valor_humedad = i["valor_humedad"]
-                humedad_dato = i["humedad_dato"]
-                valor_temperatura = i["valor_temperatura"]
-                temperatura_dato = i["temperatura_dato"]
-                humedad = i["humedad"]
-                temperatura = i["temperatura"]
-                fecha = i["fecha"]
-                listanueva.append({
-                    "clave":clave,
-                    "tipo":tipo,
-                    "descripcion":descripcion,
-                    "pin":pin,
-                    "valor_humedad":valor_humedad,
-                    "humedad_dato":humedad_dato,
-                    "valor_temperatura":valor_temperatura,
-                    "temperatura_dato":temperatura_dato,
-                    "humedad":humedad,
-                    "temperatura":temperatura,
-                    "fecha":fecha
-                    })
-            super().enviarDiccionarioYAlmacenamientoJson("dht11.json", listanueva)
-            return listanueva
-        except:
-            return False
-        
+               
         
